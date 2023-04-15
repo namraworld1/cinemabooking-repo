7a7abaec12379d8bc71158fdce3bec1ea80602c9
Namrata <namrata.rumi2290@gmail.com>
2023-04-15 10:16:11 +0100

    This is my First Git Commit

---
+++ src/main/java/com/cinema/booking/controller/BookingController.java
@@ -0,0 +1,24 @@
+package com.cinema.booking.controller;
+
+import com.cinema.booking.model.Ticket;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.ArrayList;
+
+
+@RestController
+@RequestMapping(path="/cinema")
+public class BookingController {
+    List<Ticket> ticketList=new ArrayList<>();
+    @PostMapping(path="/tickets")
+    public Ticket addTicket(@RequestBody Ticket ticket){
+        ticketList.add(ticket);
+        return ticket;
+    }
+    @GetMapping(path="/tickets")
+    public List<Ticket> getTickets(){
+        return ticketList;
+    }
+}
+
---
+++ src/test/java/com/cinema/booking/BookingApplicationTest.java
@@ -0,0 +1,18 @@
+package com.cinema.booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class BookingApplicationTest {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingApplicationTest.class, args);
+	}
+
+	/*@Test
+	void contextLoads() {
+	}
+*/
+}
